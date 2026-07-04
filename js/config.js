ac97a6ff50ca6997bedc64f515726d842d2d83ec
m-adoza <adoza004@gmail.com>
2026-07-04 17:29:04 +0100

    Remove commented-out code in config.js

--- js/config.js
+++ js/config.js
@@ -12,7 +12,6 @@ const SUPABASE_URL = 'https://ecvvxyavruvkqrinvkax.supabase.co';
 
 // Your Supabase anon public key (safe to use in browser)
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjdnZ4eWF2cnV2a3FyaW52a2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5OTc2NzQsImV4cCI6MjA5ODU3MzY3NH0.LEqGKXr1R0PrOOFhr_KePjugFVg77ruEj_DJVx9CDQw';
-
 // Initialize the Supabase client
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
